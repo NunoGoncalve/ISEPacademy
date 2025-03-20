4cb2102561ea6e411d01726703af713d6638bbd0
NunoGoncalve <1241621@isep.ipp.pt>
2025-03-20 00:22:46 +0000

    Desenvolvimento do backend do login/registo e ligação á BD

---
+++ conexao.php
@@ -0,0 +1,2 @@
+<?php $conexao = new mysqli("localhost", "DataMaster", "r_41Rmn01", "ISEPAcademy");
+mysqli_set_charset($conexao, "utf8mb4"); ?>
--- login.php
+++ login.php
@@ -1,3 +1,22 @@
+<?php 
+if(isset($POST_["email"])){
+    include 'conexao.php';
+    $email = $_POST["email"];
+	$pass = $_POST["pass"];
+	
+    $Sel="Select ID, Email, Password, Role from User where Email like '$email'";
+	$exe = mysqli_query($conexao, $Sel);
+    $row = mysqli_fetch_assoc($exe);
+    if($row["Password"]==$_POST["pass"]){
+        $_SESSION["UserID"]=$row["ID"];
+        $_SESSION["Role"]=$row["Role"];
+
+    }
+    else{
+        echo "Erro";
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
