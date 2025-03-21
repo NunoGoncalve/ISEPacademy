fbc14e3aae42a17c8a6336c8faa2f26b01df5362
Gonçalo Leite <1241608@isep.ipp.pt>
2025-03-21 19:49:51 +0000

    Página Curso + Footer para primeiro Sprint

---
+++ footer.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <footer class="footer" style="margin-top:5%">
+        <div class="columns">
+            <div class="column">
+                <h4 class="bd-footer-title has-text-weight-medium has-text-left"> 
+                        ISEP Academy
+                </h4> 
+                <p class="bd-footer-link has-text-left">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti facere necessitatibus neque repudiandae similique commodi facilis assumenda veritatis, ut animi placeat voluptatem omnis qui quod alias quaerat porro culpa error!
+                </p>
+            </div>
+            <div class="column">
+                <figure class="image" style="max-width: 35%; margin: auto;">
+                    <img src="img/01ISEPacademylogo.png" alt="logo ISEP Academy">
+                </figure>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
